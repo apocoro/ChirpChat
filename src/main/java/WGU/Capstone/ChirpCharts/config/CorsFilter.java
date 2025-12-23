acc28eae9ed3c9061a6a23ed14bf4fc4f7d4c413
Andrew W Pocoroba <65385270+Earth2Fire@users.noreply.github.com>
2025-12-23 18:01:05 -0500

    Trying to get this deployed5

--- src/main/java/WGU/Capstone/ChirpCharts/config/CorsFilter.java
+++ src/main/java/WGU/Capstone/ChirpCharts/config/CorsFilter.java
@@ -22,7 +22,7 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "https://chirpcharts.com");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "3600");
 
