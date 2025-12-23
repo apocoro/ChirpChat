0c257cd9a473614a25fc91e46e2eb42becf970c2
Andrew W Pocoroba <65385270+Earth2Fire@users.noreply.github.com>
2025-12-23 18:14:48 -0500

    Trying to get this deployed5

--- src/main/java/WGU/Capstone/ChirpCharts/controller/TeamController.java
+++ src/main/java/WGU/Capstone/ChirpCharts/controller/TeamController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
-
 @RestController
 @RequestMapping("/api/teams")
 public class TeamController {
