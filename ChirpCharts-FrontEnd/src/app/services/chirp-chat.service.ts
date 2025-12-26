524cc1afd9d1abd86bb3401c0fd7aeae5ec59df0
Andrew W Pocoroba <65385270+Earth2Fire@users.noreply.github.com>
2025-12-26 11:08:02 -0500

    Trying to get this deployed6

--- ChirpCharts-FrontEnd/src/app/services/chirp-chat.service.ts
+++ ChirpCharts-FrontEnd/src/app/services/chirp-chat.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class ChirpChatService {
 
-  private baseUrl = "http://localhost:8080/api/chirpChat"
+  private baseUrl = "https://back-end-java-springboot-chirpcharts-production.up.railway.app/api/chirpChat"
 
   constructor(private httpClient: HttpClient) { }
 
