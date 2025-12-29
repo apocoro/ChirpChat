388c7395799f319998bac62a7581980adbc536d5
Andrew W Pocoroba <65385270+Earth2Fire@users.noreply.github.com>
2025-12-28 19:18:28 -0500

    Trying to get this deployed7

--- src/main/java/WGU/Capstone/ChirpCharts/config/WebConfig.java
+++ src/main/java/WGU/Capstone/ChirpCharts/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://chirpcharts.com", "https://frontend-angular-chirpcharts-production.up.railway.app/")
+                .allowedOriginPatterns("https://chirpcharts.com", "https://frontend-angular-chirpcharts-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .maxAge(3600);
     }
